c3bf347934e39cf1fd399811343f2f7dd5a44ef8
Kornev.NE <Kornev.NE@nsk.gazprom-neft.ru>
2025-05-05 17:18:58 +0700

    add basename for Router

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/song-guesser-ui">
       <div className="app">
         <Routes>
           <Route path="/" element={<HomePage />} />
